d94f57bebd79ebca0487f3661296344e896ec399
Altaf Mahmood <altafmahmood@outlook.com>
2016-02-01 11:34:14 +0530

    Update List.h

--- src/LinkedList/List.h
+++ src/LinkedList/List.h
@@ -1,6 +1,5 @@
 //Copyright (c)
-//Authors     : Altaf Mahmood, Akash Negi & Amisha
-//Employee ID : 333841, 334085 & 334079
+//Authors     : Altaf Mahmood
 //Date        : 18 Jan 2016
 //Time        : 10:45
 //Purpose     : Program to generate a LinkedList
